aa74d17f69edd6b78e61281041bc3e1e77d41afd
Ashutosh Javheri <ashujaveri44@gmail.com>
2020-01-30 17:25:10 +0530

    Third Commit

--- SyncDemo.java
+++ SyncDemo.java
@@ -1,4 +1,35 @@
 
 public class SyncDemo implements Runnable {
 
+	@Override
+	public void run() {
+		display(Thread.currentThread().getName());
+	}
+
+	private void display(String name) {
+		System.out.println(name + " has entered...");
+
+		try {
+			synchronized (this) {
+				System.out.print("[");
+				Thread.sleep(1000);
+				System.out.print(name);
+				Thread.sleep(1000);
+				System.out.println("]");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		SyncDemo sd = new SyncDemo();
+		Thread t1 = new Thread(sd, "First");
+		Thread t2 = new Thread(sd, "Second");
+		Thread t3 = new Thread(sd, "Third");
+
+		t1.start();
+		t2.start();
+		t3.start();
+	}
 }
